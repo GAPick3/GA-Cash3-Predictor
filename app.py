be0b135a020f97f4856b5c605b70d0b2e5ef348c
GAPick3 <CedricBrooks@gmail.com>
2025-07-26 19:02:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
+# app.py
+
 from flask import Flask, render_template
-import csv
 from predictor import predict_next_numbers
+import pandas as pd
 
 app = Flask(__name__)
 
-def load_history():
-    with open("data/ga_cash3_history.csv") as f:
-        reader = csv.reader(f)
-        next(reader)  # skip header
-        return list(reader)
-
-@app.route("/")
+@app.route('/')
 def index():
-    history = load_history()
-    if not history:
-        return "No data available", 500
-    last = history[-1]
-    predictions = predict_next_numbers(history)
-    return render_template("index.html", last=last, predictions=predictions)
+    df = pd.read_csv('data/ga_cash3_history.csv')
+    latest = df.iloc[0]
+    predictions = predict_next_numbers()
+
+    return render_template("index.html", latest=latest, predictions=predictions)
 
 if __name__ == "__main__":
     app.run(debug=True)
