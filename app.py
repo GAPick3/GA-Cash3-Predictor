4126d44e2fd2164d30f4fcc1cdbcf43382c1457b
GAPick3 <CedricBrooks@gmail.com>
2025-07-26 16:49:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,5 +17,8 @@ def index():
         predictions = predict_top5(history, alpha, beta, gamma)
     return render_template('index.html', preds=predictions)
 
-if __name__=='__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
