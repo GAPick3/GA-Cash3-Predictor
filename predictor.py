12da63f9ed0fde6775c20167a2e392b490117f2f
GAPick3 <CedricBrooks@gmail.com>
2025-07-26 19:20:15 -0400

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -3,8 +3,7 @@
 import pandas as pd
 import random
 
-def predict_next_numbers(data_path='data/ga_cash3_history.csv'):
-    df = pd.read_csv(data_path)
+def predict_next_numbers(history, n=5):
     # Naive prediction: most common triplets
     freq = df['Number'].value_counts().head(5)
     return list(freq.index)
