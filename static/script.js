786c3e5276c165bfb5a81e888a5bf864d3e37e52
GAPick3 <CedricBrooks@gmail.com>
2025-07-27 23:27:11 -0400

    Delete static directory

--- static/script.js
+++
@@ -1,8 +0,0 @@
-const a=document.querySelector('input[name="alpha"]');
-const b=document.querySelector('input[name="beta"]');
-const c=document.querySelector('input[name="gamma"]');
-[a,b,c].forEach(el=>{
-  el.oninput = ()=> {
-    document.getElementById('val_'+el.name).textContent = parseFloat(el.value).toFixed(2);
-  };
-});
