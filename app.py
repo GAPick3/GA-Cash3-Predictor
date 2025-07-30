dbb893bcab5ea5630b4e92254c2fd0764ef0caaa
GAPick3 <CedricBrooks@gmail.com>
2025-07-29 23:47:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,23 @@
 from flask import Flask, render_template
 import pandas as pd
 from predictor import predict_next_numbers
-from datetime import datetime
 
 app = Flask(__name__)
 
-DATA_FILE = "data/ga_cash3_history.csv"
-
-def load_data():
-    df = pd.read_csv(DATA_FILE)
-    df['Draw Date'] = pd.to_datetime(df['Draw Date'])
-    df.sort_values(by='Draw Date', ascending=False, inplace=True)
-    return df
-
-@app.route("/")
+@app.route('/')
 def index():
-    df = load_data()
-    latest_draw = df.iloc[0]
-    
-    draw_date = latest_draw['Draw Date'].strftime('%Y-%m-%d')
+    df = pd.read_csv('data/ga_cash3_history.csv')
+    latest_draw = df.iloc[0]  # most recent row
+    draw_date = latest_draw['Draw Date']
     draw_time = latest_draw['Draw Time']
-    winning_number = latest_draw['Winning Numbers']
-
+    winning_number = f"{int(latest_draw['Digit1'])}{int(latest_draw['Digit2'])}{int(latest_draw['Digit3'])}"
     predictions = predict_next_numbers(df)
 
-    return render_template(
-        "index.html",
-        draw_date=draw_date,
-        draw_time=draw_time,
-        winning_number=winning_number,
-        predictions=predictions
-    )
+    return render_template('index.html',
+                           draw_date=draw_date,
+                           draw_time=draw_time,
+                           winning_number=winning_number,
+                           predictions=predictions)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
