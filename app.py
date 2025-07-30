0710aa1ec18e2ab464206fc0d6b6f5ffcf04e6f8
GAPick3 <CedricBrooks@gmail.com>
2025-07-29 23:11:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,36 @@
 from flask import Flask, render_template
-from predictor import predict_next_numbers
 import pandas as pd
+from predictor import predict_next_numbers
 from datetime import datetime
 
 app = Flask(__name__)
 
-HISTORY_PATH = 'data/ga_cash3_history.csv'
-PREDICTION_LOG_PATH = 'data/prediction_log.csv'
+DATA_FILE = "data/ga_cash3_history.csv"
 
-@app.route('/')
+def load_data():
+    df = pd.read_csv(DATA_FILE)
+    df['Draw Date'] = pd.to_datetime(df['Draw Date'])
+    df.sort_values(by='Draw Date', ascending=False, inplace=True)
+    return df
+
+@app.route("/")
 def index():
-    # Load latest result
-    df = pd.read_csv(HISTORY_PATH)
-    latest_draw = df.iloc[-1]
-    draw_date = latest_draw['Date']
-    draw_time = latest_draw['DrawTime']
+    df = load_data()
+    latest_draw = df.iloc[0]
+    
+    draw_date = latest_draw['Draw Date'].strftime('%Y-%m-%d')
+    draw_time = latest_draw['Draw Time']
     winning_number = latest_draw['Winning Numbers']
 
-    # Generate prediction
-    prediction = predict_next_numbers(df)
+    predictions = predict_next_numbers(df)
 
-    # Log prediction
-    log_prediction(prediction, draw_date, draw_time)
+    return render_template(
+        "index.html",
+        draw_date=draw_date,
+        draw_time=draw_time,
+        winning_number=winning_number,
+        predictions=predictions
+    )
 
-    # Load last 10 predictions
-    if os.path.exists(PREDICTION_LOG_PATH):
-        prediction_log = pd.read_csv(PREDICTION_LOG_PATH).tail(10).iloc[::-1]
-    else:
-        prediction_log = pd.DataFrame(columns=['Date', 'DrawTime', 'Prediction'])
-
-    return render_template('index.html',
-                           latest_date=draw_date,
-                           latest_time=draw_time,
-                           winning_number=winning_number,
-                           prediction=prediction,
-                           prediction_log=prediction_log)
-
-def log_prediction(prediction, date, draw_time):
-    df_log = pd.DataFrame([{
-        'Date': date,
-        'DrawTime': draw_time,
-        'Prediction': prediction
-    }])
-
-    if os.path.exists(PREDICTION_LOG_PATH):
-        df_log.to_csv(PREDICTION_LOG_PATH, mode='a', header=False, index=False)
-    else:
-        df_log.to_csv(PREDICTION_LOG_PATH, index=False)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
